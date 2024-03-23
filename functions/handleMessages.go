0f804c8d43dc36e0f5a24c40b0e896cc507d1843
SabitKabidulayev <kabidulayevs@gmail.com>
2024-03-23 16:18:46 +0500

    handleMessages

--- functions/handleMessages.go
+++ functions/handleMessages.go
@@ -2,6 +2,7 @@ package functions
 
 import (
 	"net"
+	"time"
 )
 
 type Message struct {
@@ -11,5 +12,38 @@ type Message struct {
 }
 
 func handleMessages(room *Server, conn net.Conn, ch chan<- Message) {
+	for {
 
+		timestamp := time.Now().Format("2006-01-02 15:04:05")
+		conn.Write([]byte("[" + timestamp + "]"))
+		conn.Write([]byte("[" + room.clients[conn] + "]:"))
+
+		msg, err := ReadLine(conn)
+		if err != nil {
+
+			if err.Error() == "403" {
+
+				room.mutex.Lock()
+				nameDelete := room.clients[conn]
+				delete(room.clients, conn)
+				room.mutex.Unlock()
+				connMessage := Message{
+					time:    false,
+					message: nameDelete + " has left our chat...",
+					from:    conn,
+				}
+				ch <- connMessage
+				break
+			}
+			sendErrorMessage(conn, err)
+			continue
+		}
+
+		if msg == "" {
+			continue
+		}
+
+		msgStruct := Message{true, conn, msg}
+		ch <- msgStruct
+	}
 }
